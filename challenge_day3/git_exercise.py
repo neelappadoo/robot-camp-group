79fbe030c2096bf3172f346377a079e9b9fe5f9e
Neel Appadoo <neel.appadoo@icloud.com>
2026-08-31 12:31:28 +0200

    Add team introduction exercise

---
+++ challenge_day3/git_exercise.py
@@ -0,0 +1,16 @@
+import quinn
+import neel
+import douwe
+import illya
+import mats 
+
+def introduce_team():
+    print("This is Team Junsei.")
+    print(quinn.get_name())
+    print(neel.get_name())
+    print(douwe.get_name())
+    print(illya.get_name())
+    print(mats.get_name())
+
+
+introduce_team()
