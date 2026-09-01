33916ac8dd0d4f54d43e43b9ddbd82bb275c6728
Neel Appadoo <neel.appadoo@icloud.com>
2026-09-01 10:48:13 +0200

    Complete station 6 solution

---
+++ challenge_day2/station6.py
@@ -0,0 +1,20 @@
+
+##Elimination
+##import math
+
+##x = 0.8
+
+##print("x / 2:", x / 2)
+##print("x squared:", x ** 2)
+##print("sqrt:", math.sqrt(x))
+##print("sin:", math.sin(x))
+##print("cos:", math.cos(x))
+##print("log:", math.log(x))
+
+
+
+## function code 
+import math
+
+def solution_station_6(x):
+    return math.sin(x)
