617c63cb43f53be9db2ea8dd9ed28a1df686dca0
Illya Dubitski <dbtskk@re-byods-145-3-86-203.wireless.uva.nl>
2026-09-01 12:34:56 +0200

    added station7.py

---
+++ challenge_day2/station7.py
@@ -0,0 +1,39 @@
+
+
+def solution_station_7(name: str) -> int:
+    groups = {
+        "1": [
+            "Ainas", "Tobit", "Yasmin", "Zoë",
+            "Iuliia", "Klementyna", "Markus", "Mufang", "Oumaima",
+            "Ebony", "Nandini", "Nathan", "Tiara", "Yurui",
+            "Ben", "Christopher", "Lula", "Muni", "Yuvraj",
+        ],
+        "2": [
+            "Huy", "Iris", "Katharina", "Minseo", "Sade",
+            "Alex", "Arwen", "Rajko", "Sylwia", "Zeno",
+            "Christina", "Helen", "Mark", "Mats", "Vadim",
+            "David", "Lora", "Quinn", "Tarling",
+        ],
+        "3": [
+            "Elizabeth", "Gabriel", "Jakub", "Luc", "Soelie",
+            "Aleksandra", "Arnav", "Donna", "Milan", "Rongze",
+            "Cris", "Jingqi", "Oliver", "Vaayu", "Yusef",
+            "Afua", "Anna", "Daniel", "Nataly", "Rafael",
+        ],
+        "4": [
+            "Jeremy", "Krishiv", "Neel", "Yujie", "yutong",
+            "An", "Heer", "Paige", "Samir",
+            "Amalia", "Douwe", "Illya", "Maria", "Rakin",
+            "Lara", "Lucas", "Michelle", "Oliwia", "Tom",
+        ]
+    }
+
+
+    if name in groups['1']:
+        return 1
+    elif name in groups['2']:
+        return 2
+    elif name in groups['3']:
+        return 3
+    elif name in groups['4']:
+        return 4
